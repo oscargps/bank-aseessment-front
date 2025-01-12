aab6e31a2e37a72956cfb686ea26ace922c35cf2
oscargps <oscar9557@hotmail.com>
2025-01-11 22:15:40 -0500

    ✨ Integrate cart functionality: wrap AppRouter with StoreAppProvider, add CartIcon component, and display cart item count in Products page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '../query-client';
 import { AppRouter } from './router';
+import { StoreAppProvider } from './Context/Context';
 
 export default function App() {
   return (
       <div className='MainApp h-full min-h-screen'>
         <QueryClientProvider client={queryClient}>
-          <AppRouter />
+          <StoreAppProvider>
+            <AppRouter />
+          </StoreAppProvider>
         </QueryClientProvider>
       </div>
   );
--- src/app/pages/Products.page.tsx
+++ src/app/pages/Products.page.tsx
@@ -1,25 +1,36 @@
 import { useFetchProducts } from '../hooks/useProducts';
 import { IProduct } from '../../modules/bank-store/domain/models/IProduct';
 import ProductItem from '../components/ProductItem/ProductItem.component';
+import CartIcon from '../assets/Icons/Cart';
+import { Badge } from '@nextui-org/react';
+import { useAppContext } from '../hooks/useAppContext';
 
 const Products = () => {
     const { data: products } = useFetchProducts();
+    const { cart, addToCart } = useAppContext();
 
     const handleAddToCart = (product: IProduct) => {
-        console.log('Adding to cart:', product);
+        addToCart(product);
     };
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold text-gray-900 mb-8">Products</h1>
+            <div className="flex justify-between items-center mb-8">
+                <h1 className="text-3xl font-bold text-gray-900">Products</h1>
+                <div className="flex items-center hover:cursor-pointer">
+                    <Badge color="primary" content={cart.reduce((acc, item) => acc + item.quantity, 0)}>
+                        <CartIcon />
+                    </Badge>
+                </div>
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 {products?.map((product) => (
-                <ProductItem
-                    key={product.id}
-                    product={product}
-                    onAddToCart={handleAddToCart}
-                />
-                 ))}
+                    <ProductItem
+                        key={product.id}
+                        product={product}
+                        onAddToCart={handleAddToCart}
+                    />
+                ))}
             </div>
         </div>
     );
